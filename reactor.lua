7a4572d0de3bf9bf25730e1f92bd4083479e35a5
theredstoner123 <32221378+theredstoner123@users.noreply.github.com>
2020-10-28 13:27:53 -0500

    Update reactor.lua

--- reactor.lua
+++ reactor.lua
@@ -276,10 +276,10 @@ actingAdjValue = math.abs(adjValue)
   for i=0,rodCount do
   if actingAdjValue > 0 then
 	local tempCRL = reactorRodsLevel[i]
-	local file = io.open("debug.txt","w")
-    file:write(actingAdjValue, "\n")
-    file:write(tempCRL, "\n")
-    file:close()
+	local tfile = io.open("debug.txt","w")
+    tfile:write(actingAdjValue, "\n")
+    tfile:write(tempCRL, "\n")
+    tfile:close()
 	if tempCRL <= actingAdjValue then
 		reactor.setControlRodLevel(i,0)
 		actingAdjValue = actingAdjValue - tempCRL
